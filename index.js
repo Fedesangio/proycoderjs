c27d9e4691956a14ba6f6bcdb45068a4fdb155ed
“Federico <federicosangiovanni@gmail.com>
2024-05-23 02:28:17 -0300

    JS algoritmo funcionando - HTML en proceso

--- index.js
+++ index.js
@@ -14,42 +14,52 @@ let stockfernet = 10;
 let stockwhisky = 10;
 let stockcoca = 10;
 
-let nombre = prompt ("Hola como estas? Como es tu nombre?")
+let nombre = prompt ("Hola como estas? Bienvenido a Blinders, Como es tu nombre?")
+const nacimiento = prompt (" " + nombre +  " te consulto, en que año naciste?")
 
-while (orden != 0) {
-    orden = prompt("Ingrese una opción: \n 1. Agregar un Fernet \n 2. Agregar una Whisky \n 3. Agregar una Coca-Cola \n 0. Salir");
 
-    if (orden == 1) {
-        if (ordenfernet < stockfernet){
-        alert("Agregaste un Fernet a tu carrito, su costo es de $" + Fernet + "")
-        totaldelpedido += Fernet
-        ordenfernet ++
-        }else {
-            alert ("Lo siento no tenemos mas Botellas de Fernet")
+if (nacimiento === null || isNaN(nacimiento) || nacimiento === "") {
+    alert ("Ingresa un valor correcto")
+} else {
+    let edad = 2024 - nacimiento;
+    if (edad >= 18) {
+    while (orden != 0) {
+        orden = prompt("Ingrese una opción: \n 1. Agregar un Fernet \n 2. Agregar una Whisky \n 3. Agregar una Coca-Cola \n 0. Salir");
+
+        if (orden == 1) {
+            if (ordenfernet < stockfernet){
+            alert("Agregaste un Fernet a tu carrito, su costo es de $" + Fernet + "")
+            totaldelpedido += Fernet
+            ordenfernet ++
+            }else {
+                alert ("Lo siento no tenemos mas Botellas de Fernet en el Stock")
+            }
+        } else if (orden == 2) {
+            if (ordenwhisky < stockwhisky){
+                alert("Agregaste un Whisky a tu carrito, su costo es de $" + Whisky + "")
+                totaldelpedido += Whisky
+                ordenwhisky ++
+                }else {
+                    alert ("Lo siento no tenemos mas Botellas de Whisky en el Stock")
+                }
+        } else if (orden == 3) {
+            if (ordencoca < stockcoca){
+                alert("Agregaste una Coca a tu carrito, su costo es de $" + Coca + "")
+                totaldelpedido += Coca
+                ordencoca ++
+                }else {
+                    alert ("Lo siento no tenemos mas Botellas de Coca-Cola en el Stock")
+                }
+        } else if (orden == 0) {
+            alert("" + nombre + " el total de tu pedido es de $" + totaldelpedido +". Muchas gracias por elegir Blinders! Salud!")
+        } else {
+            alert("Opcion no valida")
         }
-    } else if (orden == 2) {
-        if (ordenwhisky < stockwhisky){
-            alert("Agregaste un Whisky a tu carrito, su costo es de $" + Whisky + "")
-            totaldelpedido += Whisky
-            ordenwhisky ++
-            }else {
-                alert ("Lo siento no tenemos mas Botellas de Whisky")
-            }
-    } else if (orden == 3) {
-        if (ordencoca < stockcoca){
-            alert("Agregaste una Coca a tu carrito, su costo es de $" + Coca + "")
-            totaldelpedido += Coca
-            ordencoca ++
-            }else {
-                alert ("Lo siento no tenemos mas Botellas de Coca-Cola")
-            }
-    } else if (orden == 0) {
-        alert("Gracias por elegirnos, " + nombre + " tu pedido es de $" + totaldelpedido +"  ")
-    } else {
-        alert("Opcion no valida")
     }
+    } else {
+        alert (" " + nombre + " tenes " + edad + " años de edad, no podes entrar a esta pagina.")
 }
-
+}
 let inventario = (stock , salida ,) => console.log("Quedan en deposito " + Number(stock - salida) + " botellas de Fernet");
 let inventario1 = (stock , salida) => console.log("Quedan en deposito " + Number(stock - salida) + " botellas de Whisky");
 let inventario2 = (stock , salida) => console.log("Quedan en deposito " + Number(stock - salida) + " botellas Coca");
